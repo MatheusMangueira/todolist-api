660119bfa870e17bc0cb43c9de0ac535e3caa63a
MatheusMangueira <matheusmangueira16@hotmail.com>
2023-10-13 12:53:12 -0300

    feat: dockerfile

---
+++ src/test/java/app/vercel/matheusmangueira/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package app.vercel.matheusmangueira.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
