47e04c6e88bedc92374148210ff67268e40e24fc
MatheusMangueira <matheusmangueira16@hotmail.com>
2023-10-13 12:40:31 -0300

    feat: create userModel

---
+++ src/main/java/app/vercel/matheusmangueira/todolist/user/UserModel.java
@@ -0,0 +1,36 @@
+package app.vercel.matheusmangueira.todolist.user;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity(name = "tb_users")
+public class UserModel {
+
+   @Id
+   @GeneratedValue(generator = "UUID")
+   private UUID id;
+
+   @Column(unique = true)
+   private String username;
+   private String name;
+   private String password;
+
+   @CreationTimestamp
+   private LocalDateTime createdAt;
+
+}
